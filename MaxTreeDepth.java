028b54557cd4547415b118c45210770f58352142
Florianwi98 <florianwicher94@gmail.com>
2021-09-25 18:03:03 +0200

    MaxDepth of binTree

--- MaxTreeDepth.java
+++ MaxTreeDepth.java
@@ -1,31 +1,34 @@
 class Solution {
 
-     class TreeNode {
+    class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
-    }
 
+        public int maxDepth(TreeNode root) {
+            int rootDepth = 1;
+            if (root == null)
+                return 0;
 
+            int rightST = maxDepth(root.right);
+            int leftST = maxDepth(root.left);
 
+            int max = Math.max(rightST, leftST);
 
-    public int maxDepth(TreeNode root) {
-        int rootDepth=1;
-        if(root != null)
-            return 0;
-
-        int rightST = maxDepth(root.right);
-        int leftST = maxDepth(root.left);
-
-        int max = Math.max(rightST, leftST);
-
-        return max+rootDepth;
+            return max + rootDepth;
+        }
     }
 }
